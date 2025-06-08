39bf2fc2acb136af4a9b60a551456c2f4444515e
Raj Khare <raj.khare.java@gmail.com>
2025-06-09 05:06:38 +0530

    Programmatic Bean[registerBean("nameOfBean", BeanClass.Class, beanSupplier)]

--- code/example7/src/main/java/com/example/beans/Vehicle.java
+++ code/example7/src/main/java/com/example/beans/Vehicle.java
@@ -1,11 +1,5 @@
 package com.example.beans;
 
-
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import org.springframework.stereotype.Component;
-
-@Component
 public class Vehicle {
 
     private String name;
@@ -16,16 +10,6 @@ public class Vehicle {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @PostConstruct
-    public void initialize() {
-        this.name = "Honda";
-    }
-
-    @PreDestroy
-    public void destroy() {
-        System.out.println("Destroying Vehicle Bean");
     }
 
     public void printHello() {
