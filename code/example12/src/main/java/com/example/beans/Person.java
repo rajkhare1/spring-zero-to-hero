77642f335ef5715021e6f2eff827e3f9883c7f5c
raj.khare@oracle.com <raj.khare@oracle.com>
2025-06-13 19:41:19 +0530

    Wiring on muliple bean[@Primary, @Qualifier]

---
+++ code/example12/src/main/java/com/example/beans/Person.java
@@ -0,0 +1,51 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    private String name;
+
+    /*
+    The @Autowired annotation marks on a field, constructor, Setter method
+    is used to auto-wire the beans that is 'injecting beans' (Objects) at runtime
+    by Spring Dependency Injection mechanism
+
+    * */
+
+    /*@Autowired*/
+    /*private Vehicle vehicle;*/
+    private final Vehicle vehicle;
+
+    /*@Autowired*/ // since Spring version 4.3, if only 1 constructor, then this is optional
+    public Person(@Qualifier("vehicle2") Vehicle vehicle) {
+        System.out.println("Person bean created by Spring");
+        this.vehicle = vehicle;
+    }
+
+    @PostConstruct
+    public void initialize() {
+        this.name = "Lucy";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    /*@Autowired*/
+    /*public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }*/
+}
---
+++ code/example12/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,29 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+
+public class Vehicle {
+
+    public Vehicle() {
+        System.out.println("Vehicle bean is created by Spring");
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello() {
+        System.out.println("Printing Hello from Component Vehicle Bean");
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle name is - "+name;
+    }
+}
