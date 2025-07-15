df3b0901a4f7c432b1f06304bc732fdf26db2dfe
Raj Khare <raj.khare.java@gmail.com>
2025-07-16 05:04:36 +0530

    enhance further with th:href="@{/page}" link

---
+++ code/example_21/src/main/java/com/rajkhare/khareschool/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.rajkhare.khareschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+
+    @RequestMapping("/contact")
+    public String displayContactPage() {
+        return "contact.html";
+    }
+}
