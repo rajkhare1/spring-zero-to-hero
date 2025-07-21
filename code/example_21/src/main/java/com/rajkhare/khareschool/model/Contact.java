fd1819c26d2efb6a488ea18a0610f8c6752e7ce8
Raj Khare <raj.khare.java@gmail.com>
2025-07-22 05:13:56 +0530

    implementation of lombok

--- code/example_21/src/main/java/com/rajkhare/khareschool/model/Contact.java
+++ code/example_21/src/main/java/com/rajkhare/khareschool/model/Contact.java
@@ -1,5 +1,12 @@
 package com.rajkhare.khareschool.model;
 
+import lombok.Data;
+/*
+ @Data annotation is provided by Lombok library which generates:
+ getter, setter, equals(), toString() methods & Constructor at compile time.
+ This makes our code short and clean
+* */
+@Data
 public class Contact {
 
     private String name;
@@ -8,54 +15,4 @@ public class Contact {
     private String subject;
     private String message;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMobileNum() {
-        return mobileNum;
-    }
-
-    public void setMobileNum(String mobileNum) {
-        this.mobileNum = mobileNum;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "name='" + name + '\'' +
-                ", mobileNum='" + mobileNum + '\'' +
-                ", email='" + email + '\'' +
-                ", subject='" + subject + '\'' +
-                ", message='" + message + '\'' +
-                '}';
-    }
 }
