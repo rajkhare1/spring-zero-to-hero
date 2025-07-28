37e2ba30b5ec9e300bceaad3a71148925405af7f
Raj Khare <raj.khare.java@gmail.com>
2025-07-29 05:05:57 +0530

    adding template project for Java Bean validation

---
+++ code/example_24/src/main/java/com/rajkhare/khareschool/service/ContactService.java
@@ -0,0 +1,18 @@
+package com.rajkhare.khareschool.service;
+
+import com.rajkhare.khareschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ContactService {
+
+    public boolean saveMessageDetails(Contact contact) {
+        boolean isSaved = true;
+        //TODO - need to persist the data into DB table
+        log.info(contact.toString());
+        return isSaved;
+    }
+
+}
