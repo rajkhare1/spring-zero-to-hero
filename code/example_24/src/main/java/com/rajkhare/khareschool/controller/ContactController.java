ced5397e7bc0e32594c00676f29f48dadf3bd096
Raj Khare <raj.khare.java@gmail.com>
2025-08-03 05:49:39 +0530

    changes to implement the Bean Validation

--- code/example_24/src/main/java/com/rajkhare/khareschool/controller/ContactController.java
+++ code/example_24/src/main/java/com/rajkhare/khareschool/controller/ContactController.java
@@ -2,10 +2,14 @@ package com.rajkhare.khareschool.controller;
 
 import com.rajkhare.khareschool.model.Contact;
 import com.rajkhare.khareschool.service.ContactService;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,7 +30,8 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
@@ -43,9 +48,13 @@ public class ContactController {
     }*/
 
     @RequestMapping(value = "/saveMsg", method = POST)
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if(errors.hasErrors()) {
+            log.error("Contact form validation failed due to : {}", errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 
 }
