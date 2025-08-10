bbc94dac567ad27bbc7e1071a0ccc54966d61f18
Raj Khare <raj.khare.java@gmail.com>
2025-08-11 05:02:45 +0530

    Implementing Spring Security[authenticated(), permitAll(), denyAll()]

---
+++ code/example_27/src/main/java/com/rajkhare/khareschool/KhareSchoolApplication.java
@@ -0,0 +1,15 @@
+package com.rajkhare.khareschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class KhareSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhareSchoolApplication.class, args);
+	}
+
+}
---
+++ code/example_27/src/main/java/com/rajkhare/khareschool/service/ContactService.java
@@ -0,0 +1,37 @@
+package com.rajkhare.khareschool.service;
+
+import com.rajkhare.khareschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Slf4j
+@Service
+//@RequestScope
+//@SessionScope
+@ApplicationScope
+public class ContactService {
+
+    private int counter = 0;
+
+    public ContactService() {
+        System.out.println("Contact Service Bean initialized");
+    }
+
+    public boolean saveMessageDetails(Contact contact) {
+        boolean isSaved = true;
+        //TODO - need to persist the data into DB table
+        log.info(contact.toString());
+        return isSaved;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+}
