242908e6853a74cf099e09a5564e467d1a9d5207
Raj Khare <raj.khare.java@gmail.com>
2025-08-16 05:28:43 +0530

    change for the integration for the Thymeleaf and Spring security

---
+++ code/example_28/src/main/java/com/rajkhare/khareschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.rajkhare.khareschool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class ProjectSecurityConfig {
+    /*
+    * /courses
+    * /about
+    * /contact
+    * /saveMsg
+    * /holidays
+    * /home
+    *
+    * */
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests(
+                        (requests) -> requests.requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/holidays/**").permitAll()
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/saveMsg").permitAll()
+                        .requestMatchers("/courses").permitAll()
+                        .requestMatchers("/about").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    }
+
+
