ed619f0d43f1e313a210d44026d3449a048a0361
Raj Khare <raj.khare.java@gmail.com>
2025-09-08 05:23:16 +0530

    Migration[from h2 to mySql in aws]

---
+++ code/example_34/src/main/java/com/rajkhare/khareschool/service/ContactService.java
@@ -0,0 +1,52 @@
+package com.rajkhare.khareschool.service;
+
+import com.rajkhare.khareschool.constant.EazySchoolConstants;
+import com.rajkhare.khareschool.models.Contact;
+import com.rajkhare.khareschool.repository.ContactRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+
+    public ContactService() {
+        System.out.println("Contact Service Bean initialized");
+    }
+
+    public boolean saveMessageDetails(Contact contact) {
+        boolean isSaved = false;
+        contact.setStatus(EazySchoolConstants.OPEN);
+        contact.setCreatedBy(EazySchoolConstants.ANONYMOUS);
+        contact.setCreatedAt(LocalDateTime.now());
+        int result = contactRepository.saveContactMsg(contact);
+        if(result>0) {
+            isSaved = true;
+        }
+        return isSaved;
+    }
+
+    public List<Contact> findMsgsWithOpenStatus(){
+        List<Contact> contactMsgs = contactRepository.findMsgsWithStatus(EazySchoolConstants.OPEN);
+        return contactMsgs;
+    }
+
+    public boolean updateMsgStatus(int contactId, String updatedBy) {
+        boolean isUpdated = false;
+        int result = contactRepository.updateMsgStatus(contactId,EazySchoolConstants.CLOSE,updatedBy);
+        if(result>0) {
+            isUpdated = true;
+        }
+        return isUpdated;
+    }
+
+}
