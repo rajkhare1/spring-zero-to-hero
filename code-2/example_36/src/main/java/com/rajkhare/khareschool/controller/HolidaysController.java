8ead63cf81fa2bfba92ea13fd741207e37f599f7
Raj Khare <raj.khare.java@gmail.com>
2025-09-21 03:50:12 +0530

    Auditing support for Spring Data JPA

---
+++ code-2/example_36/src/main/java/com/rajkhare/khareschool/controller/HolidaysController.java
@@ -0,0 +1,41 @@
+package com.rajkhare.khareschool.controller;
+
+import com.rajkhare.khareschool.model.Holiday;
+import com.rajkhare.khareschool.repository.HolidaysRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Controller
+public class HolidaysController {
+
+    @Autowired
+    private HolidaysRepository holidaysRepository;
+
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display, Model model) {
+        if(null != display && display.equals("all")) {
+            model.addAttribute("festival", true);
+            model.addAttribute("federal", true);
+        } else if(null != display && display.equals("federal")) {
+            model.addAttribute("federal", true);
+        } else if(null != display && display.equals("festival")) {
+            model.addAttribute("festival", true);
+        }
+        Iterable<Holiday> holidaysIterable = holidaysRepository.findAll();
+        List<Holiday> holidays = StreamSupport.stream(holidaysIterable.spliterator(), false).collect(Collectors.toList());
+        Holiday.Type[] types = Holiday.Type.values();
+        for(Holiday.Type type : types) {
+            model.addAttribute(type.toString(),
+                    (holidays.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
+        }
+        return "holidays.html";
+    }
+
+}
