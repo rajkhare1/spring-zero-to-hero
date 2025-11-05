a137324fcd6ec8e20b71fea2dbfc40f52b865c54
Raj Khare <raj.khare.java@gmail.com>
2025-11-06 05:07:22 +0530

    custom authentication implementation for db authentication

--- code-2/example_38/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ code-2/example_38/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -38,20 +38,4 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("54321")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-
 }
