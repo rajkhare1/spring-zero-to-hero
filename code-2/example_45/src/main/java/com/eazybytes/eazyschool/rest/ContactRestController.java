3a26b1ffd85949dd5746c1d9abc84ffc4c080810
Raj Khare <raj.khare.java@gmail.com>
2025-12-03 10:46:42 +0530

    working with rest services

---
+++ code-2/example_45/src/main/java/com/eazybytes/eazyschool/rest/ContactRestController.java
@@ -0,0 +1,36 @@
+package com.eazybytes.eazyschool.rest;
+
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.repository.ContactRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping(path = "/api/contact")
+public class ContactRestController {
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    @GetMapping("/getMessagesByStatus")
+    @ResponseBody
+    public List<Contact> getMessagesByStatus(@RequestParam(name = "status") String status) {
+        return contactRepository.findByStatus(status);
+    }
+
+    @GetMapping("/getAllMsgsByStatus")
+    @ResponseBody
+    public List<Contact> getAllMsgsByStatus(@RequestBody Contact contact){
+        if(null != contact && null != contact.getStatus()){
+            return contactRepository.findByStatus(contact.getStatus());
+        }else{
+            return List.of();
+        }
+    }
+
+}
